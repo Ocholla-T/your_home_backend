0ac7d7a8ce400ceda573fa7afac887aaa1e3ec8e
Ocholla-T <tonnyocholla8@gmail.com>
2021-06-17 18:23:05 +0300

    added methods to create, read, and delete devices

--- server/api/controller/device.controller.js
+++ server/api/controller/device.controller.js
@@ -1,13 +1,29 @@
-const mongoose = require('mongoose');
-const Devices = mongoose.model('Devices');
+import { Device } from '../models/device.model';
 
-exports.list_all_devices = (req, res) => {
-  Devices.find(async (err, device) => {
-    try {
-      await res.json(device);
-    } catch (error) {
-      console.error('Error finding devices:', error);
-      res.send(error);
-    }
+export const listAllDevices = (req, res) => {
+  const allDevices = await Device.find({}).catch((error) => console.error(error));
+
+  res.json(allDevices);
+};
+
+export const readOneDevice = (req, res) => {
+  const device = await Device.findById(id).catch((error) => console.error(error));
+
+  res.json(device);
+};
+
+export const createDevice = (req, res) => {
+  const device = new Device({
+    device: req.body.device,
+    buyingPrice: req.body.buyingPrice,
+    averageLifespan: req.body.averageLifespan,
+    yearsInUse: req.body.yearsInUse,
   });
+
+  const newDevice = await device.save().catch((error) => console.error(error));
+  res.json(newDevice);
+};
+
+export const deleteOneDevice = (req, res) => {
+  await Device.deleteOne({ _id: req.body.device.id }).catch((error) => console.error(error));
 };
