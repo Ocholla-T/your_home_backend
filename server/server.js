157a9ac584d46b33606fb88725fb8938c24de043
Ocholla-T <tonnyocholla8@gmail.com>
2021-06-18 17:16:00 +0300

    added mongodb database connection

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
-const express = require('express');
-require('dotenv').config();
-const app = express();
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import routes from './api/routes/yourHome.routes.js';
 
-const port = process.env.PORT || 3000;
+dotenv.config();
 
-app.get('/', (req, res) => {
-  res.send(`tumereach`);
+global.Device = import('./api/models/device.model.js');
+mongoose.connect('mongodb://localhost:27017/your_home_DB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`server started on port ${port}`);
 });
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+routes(app);
